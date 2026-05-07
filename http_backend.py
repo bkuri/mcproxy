83b4f6284bd76b40cc4650a870af9cdde01a54db
Bernardo Kuri <github@bkuri.com>
2026-05-07 15:54:54 -0600

    chore: bump version to 5.1.0 for search result cache feature

--- http_backend.py
+++ http_backend.py
@@ -73,7 +73,7 @@ class HTTPServerConnector:
                 params={
                     "protocolVersion": "2024-11-05",
                     "capabilities": {},
-                    "clientInfo": {"name": "mcproxy", "version": "5.0.0"},
+                    "clientInfo": {"name": "mcproxy", "version": "5.1.0"},
                 },
                 id="init",
             )
